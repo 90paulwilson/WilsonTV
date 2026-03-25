65293b405244eb0c6cdda4e24a52b87cdc77bf2f
90paulwilson <164881745+90paulwilson@users.noreply.github.com>
2026-03-25 11:36:31 +0000

    Change default theme from WHITE to OCEAN

--- app/src/main/java/com/nuvio/tv/ui/theme/Theme.kt
+++ app/src/main/java/com/nuvio/tv/ui/theme/Theme.kt
@@ -37,12 +37,11 @@ val LocalNuvioExtendedColors = staticCompositionLocalOf {
     )
 }
 
-val LocalAppTheme = staticCompositionLocalOf { AppTheme.WHITE }
-
+val LocalAppTheme = staticCompositionLocalOf { AppTheme.OCEAN }
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun NuvioTheme(
-    appTheme: AppTheme = AppTheme.WHITE,
+    appTheme: AppTheme = AppTheme.OCEAN,
     appFont: AppFont = AppFont.INTER,
     content: @Composable () -> Unit
 ) {
